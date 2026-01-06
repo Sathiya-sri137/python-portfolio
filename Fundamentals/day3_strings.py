8ad4e98d1093bb96f9734871e6fff6c0fc60e930
sathiya <your -email@example.com>
2026-01-06 17:17:30 +0530

    Day 3: Python strings practice

---
+++ Fundamentals/day3_strings.py
@@ -0,0 +1,18 @@
+name="Sathiya Sri"
+language= "Python"
+print(name)
+print(language)
+print(language[0])
+print(language[-1])
+print(name[1:9])
+print(name[:7])
+print(name[0:])
+print(name[:])
+print('sri' in name)
+print('n' in name)
+details = name + " and " + language
+num1=10
+num2=8
+print(f'The sum of {num1} and {num2} is {num1 + num2}')
+print(name[::-1])
+print('sathiya sri'.replace('sri', 'devaraj'))
