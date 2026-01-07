9fc405ff121411ed2bb1a29360923255f45d729e
sathiya <your -email@example.com>
2026-01-07 18:42:09 +0530

    Day 4: Numbers and Math operations

---
+++ Fundamentals/day4_numbers.py
@@ -0,0 +1,17 @@
+#Day 4 - Numbers and Math Operations
+x = 10
+y = 5
+print("Addition:", x + y)
+print("Subraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+print("Floor Division:", x // y)
+print("Power:", x ** y)
+a = 4.5
+b = 1.2
+print("Module:", a % b)
+print("Rounded value:", round(1.2))
+print("Absolute value:", abs(-45))
+print("Power:", pow(2,3,5))
+print("Integer:", int(45.9))
+print("Float:",float(89))
